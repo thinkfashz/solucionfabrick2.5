56275c5a6b4f6de0126577c3f7625d5c29d98ee0
thinkfashz <faubricioedms@gmail.com>
2026-06-11 06:07:26 -0400

    Add radier budget engine admin page

---
+++ src/app/admin/motores/radier/page.tsx
@@ -0,0 +1,10 @@
+import dynamic from 'next/dynamic';
+
+const FabrickBudgetEnginesClient = dynamic(() => import('@/components/admin/motores/FabrickBudgetEnginesClient'), {
+  ssr: false,
+  loading: () => <main className="min-h-screen bg-[#090806] p-6 text-white">Cargando motor de radier…</main>,
+});
+
+export default function MotorRadierPage() {
+  return <FabrickBudgetEnginesClient kind="radier" />;
+}

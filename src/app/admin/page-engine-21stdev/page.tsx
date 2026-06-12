f0d149f591f44ebdb659ed56160cb623a1686610
thinkfashz <faubricioedms@gmail.com>
2026-06-11 21:27:24 -0400

    Show generated HTML links history on page engine route

--- src/app/admin/page-engine-21stdev/page.tsx
+++ src/app/admin/page-engine-21stdev/page.tsx
@@ -4,6 +4,7 @@ import dynamic from 'next/dynamic';
 import PageEngineMobileDrawerEnhancer from '@/components/admin/page-engine/PageEngineMobileDrawerEnhancer';
 import PageEngineLiveExpiryBadge from '@/components/admin/page-engine/PageEngineLiveExpiryBadge';
 import PageEngineShopifyChrome from '@/components/admin/page-engine/PageEngineShopifyChrome';
+import PageEngineLinksHistory from '@/components/admin/page-engine/PageEngineLinksHistory';
 
 const AdvancedHtmlPageEngineClientV2 = dynamic(() => import('@/components/admin/page-engine/AdvancedHtmlPageEngineClientV2'), {
   ssr: false,
@@ -15,6 +16,7 @@ export default function PageEngine21stDevPage() {
     <PageEngineShopifyChrome />
     <PageEngineMobileDrawerEnhancer />
     <AdvancedHtmlPageEngineClientV2 />
+    <PageEngineLinksHistory />
     <PageEngineLiveExpiryBadge />
   </>;
 }

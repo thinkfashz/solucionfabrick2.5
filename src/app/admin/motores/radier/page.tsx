066542c9f9d58026e660ffa0eceee668bbd9a09d
thinkfashz <faubricioedms@gmail.com>
2026-06-11 21:28:00 -0400

    Show budget links history on Radier motor route

--- src/app/admin/motores/radier/page.tsx
+++ src/app/admin/motores/radier/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import dynamic from 'next/dynamic';
+import BudgetLinksHistory from '@/components/admin/motores/BudgetLinksHistory';
 
 const FabrickBudgetEnginesPremiumClient = dynamic(() => import('@/components/admin/motores/FabrickBudgetEnginesPremiumClient'), {
   ssr: false,
@@ -8,5 +9,8 @@ const FabrickBudgetEnginesPremiumClient = dynamic(() => import('@/components/adm
 });
 
 export default function MotorRadierPage() {
-  return <FabrickBudgetEnginesPremiumClient kind="radier" />;
+  return <>
+    <FabrickBudgetEnginesPremiumClient kind="radier" />
+    <BudgetLinksHistory />
+  </>;
 }

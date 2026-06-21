f792cbf92fde84bbcee6377192f92961912711bb
thinkfashz <faubricioedms@gmail.com>
2026-06-21 01:08:30 -0400

    Usa motor mejorado de radier

--- src/app/admin/motores/radier/page.tsx
+++ src/app/admin/motores/radier/page.tsx
@@ -3,14 +3,14 @@
 import dynamic from 'next/dynamic';
 import BudgetLinksHistory from '@/components/admin/motores/BudgetLinksHistory';
 
-const FabrickBudgetEnginesPremiumClient = dynamic(() => import('@/components/admin/motores/FabrickBudgetEnginesPremiumClient'), {
+const RadierBudgetEngineLite = dynamic(() => import('@/components/admin/motores/RadierBudgetEngineLite'), {
   ssr: false,
-  loading: () => <main className="min-h-screen bg-[#050403] p-6 text-white">Cargando motor premium de radier…</main>,
+  loading: () => <main className="min-h-screen bg-[#050403] p-6 text-white">Cargando motor mejorado de radier…</main>,
 });
 
 export default function MotorRadierPage() {
   return <>
-    <FabrickBudgetEnginesPremiumClient kind="radier" />
+    <RadierBudgetEngineLite />
     <BudgetLinksHistory />
   </>;
 }

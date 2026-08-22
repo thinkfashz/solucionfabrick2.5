f549e55dd5a508a91959a67eeac49af3d711cd6a
thinkfashz <faubricioedms@gmail.com>
2026-08-22 13:16:35 -0400

    refactor(admin): remove redundant theme frame wrapper

--- src/components/admin/AdminShellSwitcher.tsx
+++ src/components/admin/AdminShellSwitcher.tsx
@@ -1,13 +1,10 @@
 import type { ReactNode } from 'react';
-import AdminBaseThemeFrame from '@/components/admin/AdminBaseThemeFrame';
 import { AdminShell } from '@/components/admin/AdminShell';
 
 export function AdminShellSwitcher({ children }: { children: ReactNode }) {
   return (
     <div id="shell-fabrick" className="fabrick-shell" data-admin-shell="fabrick">
-      <AdminBaseThemeFrame>
-        <AdminShell>{children}</AdminShell>
-      </AdminBaseThemeFrame>
+      <AdminShell>{children}</AdminShell>
     </div>
   );
 }

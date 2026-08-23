b5fea3df225933f32944d0ffd0ea5c589e4b2166
thinkfashz <faubricioedms@gmail.com>
2026-08-23 02:38:06 -0400

    chore(admin): remove unused legacy studio exports

--- src/components/admin-studio/index.ts
+++
@@ -1,4 +0,0 @@
-export { StudioShell } from './StudioShell';
-export { StudioSidebar, StudioSidebarContent } from './StudioSidebar';
-export { StudioHeader } from './StudioHeader';
-export { navSections } from './StudioSidebar';

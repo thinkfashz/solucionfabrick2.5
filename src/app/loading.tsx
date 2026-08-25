d1fa7ae427f75cdc89dd7e6c20657b74cf75ca50
thinkfashz <faubricioedms@gmail.com>
2026-08-25 19:22:39 -0400

    design(brand): add global Fabrick loading screen

---
+++ src/app/loading.tsx
@@ -0,0 +1,11 @@
+import FabrickLoadingScreen from '@/components/FabrickLoadingScreen';
+
+export default function GlobalLoading() {
+  return (
+    <FabrickLoadingScreen
+      eyebrow="Soluciones Fabrick"
+      title="Cargando experiencia"
+      description="Preparando la aplicación y sus servicios."
+    />
+  );
+}
